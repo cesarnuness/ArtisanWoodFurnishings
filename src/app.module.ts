ac03bb0291cc8f531df0c0bf4439786ca20f22eb
CESAR NUNES DE OLIVEIRA <ra22139193-5@aluno.unicesumar.edu.br>
2023-08-29 08:59:13 -0300

    Basic setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { CategoryModule } from './category/category.module';
+import { ProductModule } from './product/product.module';
+import { CartModule } from './cart/cart.module';
+import { OrderModule } from './order/order.module';
+import { ElasticSearchModule } from './elastic-search/elastic-search.module';
+import { PaymentModule } from './payment/payment.module';
+import { AwsDeploymentModule } from './aws-deployment/aws-deployment.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, CategoryModule, ProductModule, CartModule, OrderModule, ElasticSearchModule, PaymentModule, AwsDeploymentModule],
   controllers: [AppController],
   providers: [AppService],
 })
