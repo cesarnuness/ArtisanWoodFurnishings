ae209aefc99f63eaaf5a164cb60b2c7790b0555e
CESAR NUNES DE OLIVEIRA <ra22139193-5@aluno.unicesumar.edu.br>
2023-08-31 18:02:17 -0300

    added a couple models to prisma schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,24 +3,24 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { CategoryModule } from './category/category.module';
-import { ProductModule } from './product/product.module';
 import { CartModule } from './cart/cart.module';
 import { OrderModule } from './order/order.module';
 import { ElasticSearchModule } from './elastic-search/elastic-search.module';
 import { PaymentModule } from './payment/payment.module';
 import { AwsDeploymentModule } from './aws-deployment/aws-deployment.module';
+import { ProductsModule } from './products/products.module';
 import TestController from './test/test.controller';
 
 @Module({
   imports: [
     AuthModule,
     CategoryModule,
-    ProductModule,
     CartModule,
     OrderModule,
     ElasticSearchModule,
     PaymentModule,
     AwsDeploymentModule,
+    ProductsModule,
   ],
   controllers: [AppController, TestController],
   providers: [AppService],
