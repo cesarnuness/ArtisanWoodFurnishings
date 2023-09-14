bb7bd09766593faa1e09071503c4f24a4b26d1a7
César Nunes <cesar.nunes@live.com>
2023-09-14 17:04:18 -0300

    Finished primary dto for products

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,32 @@
-export class CreateProductDto {}
+import {
+  IsString,
+  IsNumber,
+  IsBoolean,
+  IsInt,
+  Max,
+  Min,
+} from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  name: string;
+
+  @IsString()
+  description: string;
+
+  @IsNumber()
+  @Min(0.01, { message: 'Preço precisa ser de no mínimo 0.01 reais' })
+  @Max(9999.99, { message: 'Valor no máximo de 9999.99' })
+  price: number;
+
+  @IsString()
+  photo: string;
+
+  @IsBoolean()
+  published: boolean;
+
+  @IsInt()
+  @Min(0, { message: 'Valor precisa ser no mínimo 0' })
+  @Max(9999, { message: 'Valor pode ser no máximo 9999' })
+  quantity: number;
+}
